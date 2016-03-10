160a0ad0aed37d9506522b0da9b02534118375f3
Andrey Adamovich <andrey@aestasit.com>
2016-03-10 13:50:35 +0200

    updates

--- elastic-test-suite/src/test/groovy/org/javaland/elasticsearch/PetclinicTest.groovy
+++ elastic-test-suite/src/test/groovy/org/javaland/elasticsearch/PetclinicTest.groovy
@@ -1,7 +1,10 @@
 package org.javaland.elasticsearch
 
 import groovy.util.logging.Slf4j
+import org.elasticsearch.action.delete.DeleteResponse
 import org.elasticsearch.action.index.IndexResponse
+import org.elasticsearch.action.update.UpdateResponse
+import org.elasticsearch.client.transport.TransportClient
 import org.junit.Test
 
 @Slf4j
@@ -13,7 +16,25 @@ public class PetclinicTest extends ElasticsearchTest {
     IndexResponse response = client.prepareIndex("petclinic", "vet", "1")
       .setSource(source)
       .get()
-    response.getId()
+    println response.getId()
   }
+
+  @Test
+  public void updateTest() {
+    String source = '{ "user" : "Andrey", "post_date" : "2009-11-15T14:12:12" }'
+    UpdateResponse response = client.prepareUpdate("petclinic", "vet", "1")
+      .setDoc(source.bytes)
+      .get()
+    println response.getId()
+  }
+
+
+  @Test
+  public void deleteTest() {
+    String source = '{ "user" : "Andrey", "post_date" : "2009-11-15T14:12:12" }'
+    DeleteResponse response = client.prepareDelete("petclinic", "vet", "1").get()
+    println response.getId()
+  }
+
 
 }
